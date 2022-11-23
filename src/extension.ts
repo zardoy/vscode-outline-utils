0ef2c63ef2122fe7e4fa57bef8f82f6d70e59813
Vitaly Turovsky <vital2580@icloud.com>
2022-11-24 01:58:47 +0300

    initial release!

---
+++ src/extension.ts
@@ -0,0 +1,11 @@
+import copyCurrentOutlinePath from "./commands/copyCurrentOutlinePath"
+import copyOutlineItemName from "./commands/copyOutlineItemName"
+import openOutlineItemInNewEditor from "./commands/openOutlineItemInNewEditor"
+import selectOutlineItem from "./commands/selectOutlineItem"
+
+export const activate = () => {
+	copyOutlineItemName()
+    selectOutlineItem()
+    copyCurrentOutlinePath()
+    openOutlineItemInNewEditor()
+}
